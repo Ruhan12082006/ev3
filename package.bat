f7d0c53c39764afb1410412230cbd08551a9d79f
Ruhan12082006 <ruhanghantiwala@gmail.com>
2026-03-30 00:27:41 +0300

    First did the half part of the task

--- package.bat
+++ package.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe UltrasonicSensor.jar src.UltrasonicSensor -C . src
+"C:\Program Files\Java\jdk1.7.0_80\bin\jar" -cfe UltraS.jar src.UltraS -C . src
--- src/UltraS.java
+++ src/UltraS.java
@@ -9,7 +9,7 @@ import lejos.hardware.Button;
 import lejos.robotics.SampleProvider;   // allows the sensor to return the samples or data
                                         // e.g., for getting distance data from sonic sensor etc
 
-public class UltrasonicSensor {
+public class UltraS {
 
     public static void main(String[] args) {
         // Creating an instance of US sensor at port 2
@@ -19,24 +19,20 @@ public class UltrasonicSensor {
         SampleProvider distance = ultrasonicSensor.getDistanceMode();
         LCD.clear();
 
-        
-
-
         // Create a sample array to hold the distance value
         // even though sonic sensor gives distance as an o/p, but since other sensors, e.g., light sensor
         // can provide multiple values, therefore to keep consistency, I'm using sampleprovider
         float[] sample = new float[distance.sampleSize()];
-    
-        // Keep displaying the distance, until user presses a button
-        while (!Button.ESCAPE.isDown())
-        {
-            Motor.A.setSpeed(360); 
+
+         Motor.A.setSpeed(360); 
             Motor.B.setSpeed(360);
             Motor.A.forward();
             Motor.B.forward();
             Delay.msDelay(1000);
-
-            Delay.msDelay(1000);
+    
+        // Keep displaying the distance, until user presses a button
+        while (!Button.ESCAPE.isDown())
+        {
             
             // Get the curRent distnce reading from the US sensor
             distance.fetchSample(sample, 0);
@@ -44,13 +40,30 @@ public class UltrasonicSensor {
             // Display the distance on the LCD screen
             LCD.clear();
             LCD.drawString("Dist: " + sample[0] + " meters", 0, 0);
-            
-            while (sample[0] < 0.3 && sample[0] > 0.1)
+
+            //try-1
+            //first we have tried while loop 👇
+            // while (sample[0] < 0.3 && sample[0] > 0.1)
+            // {
+            //     Motor.A.setSpeed(100);
+            //     Motor.B.setSpeed(100);
+            // }
+            // if (sample[0] < 0.1){
+            //     Motor.A.stop(true);  
+            //     Motor.B.stop(true);
+            // }
+
+            //try-2
+            if (sample[0] < 0.5 && sample[0] > 0.3)
             {
                 Motor.A.setSpeed(100);
                 Motor.B.setSpeed(100);
+                Motor.A.forward();
+                Motor.B.forward();
             }
-            if (sample[0] < 0.1){
+
+            if (sample[0] <= 0.3)
+            {
                 Motor.A.stop(true);  
                 Motor.B.stop(true);
             }
@@ -65,5 +78,6 @@ public class UltrasonicSensor {
         
         // Close US sensor
         ultrasonicSensor.close();
+        Button.waitForAnyPress();
     }
 }
